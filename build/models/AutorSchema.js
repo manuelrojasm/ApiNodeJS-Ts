3b062ddf7fdf9c5a4fac0005c681df69a0520b21
manuelrojasm <manuelrojasm13@gmail.com>
2022-06-06 20:13:07 -0500

    fixed project

--- build/models/AutorSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const AutorSchema = new mongoose_1.Schema({
-    name: { type: String, required: true },
-    lastName: { type: String, required: true },
-    CreateAt: { type: Date, default: Date.now() }
-});
-exports.default = (0, mongoose_1.model)("Autor", AutorSchema);
